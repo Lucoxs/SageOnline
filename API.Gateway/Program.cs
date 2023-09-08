ace13530e96aff806c2ed5b21d0a8db3c85aa5b0
LucasStrohl <strohllucas93160@gmail.com>
2023-09-08 09:39:07 +0200

    base gateway

---
+++ API.Gateway/Program.cs
@@ -0,0 +1,38 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:7001/";
+        options.TokenValidationParameters = new()
+        {
+            ValidateAudience = false
+        };
+    });
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.UseOcelot();
+
+app.Run();
